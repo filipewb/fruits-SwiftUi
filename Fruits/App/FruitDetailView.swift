efa323962c889bca64ec8ee693b84e41fff46f1c
filipeboeck <filipe.boeck@hotmail.com>
2023-06-13 20:51:00 -0300

    feat: code

---
+++ Fruits/App/FruitDetailView.swift
@@ -0,0 +1,25 @@
+//
+//  FruitDetailView.swift
+//  Fruits
+//
+//  Created by Filipe Boeck on 13/06/23.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - Preview
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView()
+    }
+}
