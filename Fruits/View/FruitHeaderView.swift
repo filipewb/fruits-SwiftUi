7a451f65a92aca1645a6d2e4b7f7373a2d59313b
filipeboeck <filipe.boeck@hotmail.com>
2023-07-26 16:13:11 -0300

    fix: ajuste de icone

---
+++ Fruits/View/FruitHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitHeaderView.swift
+//  Fruits
+//
+//  Created by Filipe Boeck on 26/07/23.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitHeaderView()
+    }
+}
---
+++ Fruits/View/SourceLinkView.swift
@@ -0,0 +1,20 @@
+//
+//  SourceLinkView.swift
+//  Fruits
+//
+//  Created by Filipe Boeck on 26/07/23.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SourceLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        SourceLinkView()
+    }
+}
