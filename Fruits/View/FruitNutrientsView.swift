c453ef15d0a86c868b6724fbbe0186355bcf0c0f
filipeboeck <filipe.boeck@hotmail.com>
2023-07-26 16:32:47 -0300

    feat: criacao da tela de nutrientes

---
+++ Fruits/View/FruitNutrientsView.swift
@@ -0,0 +1,50 @@
+//
+//  FruitNutrientsView.swift
+//  Fruits
+//
+//  Created by Filipe Boeck on 26/07/23.
+//
+
+import SwiftUI
+
+struct FruitNutrientsView: View {
+    // MARK: - PROPERTIES
+    
+    var fruit: Fruit
+    var nutrients: [String] = ["Energy", "Sugar", "Fat", "Protein", "Vitamins", "Minerals"]
+    
+    // MARK: - BODY
+    var body: some View {
+        GroupBox() {
+            DisclosureGroup("Nutritional value per 100g") {
+                ForEach(0..<nutrients.count, id: \.self) { item in
+                    Divider().padding(.vertical, 2)
+                    
+                    HStack {
+                        Group {
+                            Image(systemName: "info.circle")
+                            Text(nutrients[item])
+                        }
+                        .foregroundColor(fruit.gradientColors[1])
+                        .font(Font.system(.body).bold())
+                        
+                        Spacer(minLength: 25)
+                        
+                        Text(fruit.nutrition[item])
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+            }
+        }
+    }
+}
+
+// MARK: - PREVIEW
+
+struct FruitNutrientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitNutrientsView(fruit: fruitsData[0])
+            .previewLayout(.fixed(width: 375, height: 480))
+            .padding()
+    }
+}
