10ccb0d9b987fbbfe989c69cac5d82afe2a00ffd
filipeboeck <filipe.boeck@hotmail.com>
2023-07-28 09:56:35 -0300

    feat: criacao da settingsLabelView

---
+++ Fruits/App/SettingsView.swift
@@ -0,0 +1,60 @@
+//
+//  SettingsView.swift
+//  Fruits
+//
+//  Created by Filipe Boeck on 26/07/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - Properties
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    // MARK: - Body
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 20) {
+                    
+                    GroupBox(
+                        label:
+                            SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                            
+                        HStack(alignment: .center, spacing: 10) {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            
+                            Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins, and much more.")
+                                .font(.footnote)
+                        }
+                    }
+                }
+                .navigationBarTitle(Text("Settings"), displayMode: .large)
+                .navigationBarItems(
+                    trailing:
+                        Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Image(systemName: "xmark")
+                        }
+                )
+                .padding()
+            }
+        }
+    }
+}
+
+// MARK: - Preview
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
