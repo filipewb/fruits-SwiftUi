ef318791ba94c97cbd17590fc08ca0dbfae84505
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-09-04 18:55:35 -0300

    feat:
    adicionando novas informacoes nas telas

--- Fruits/App/FruitDetailView.swift
+++ Fruits/App/FruitDetailView.swift
@@ -51,6 +51,7 @@ struct FruitDetailView: View {
             }
             .edgesIgnoringSafeArea(.top)
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
--- Fruits/App/SettingsView.swift
+++ Fruits/App/SettingsView.swift
@@ -11,6 +11,7 @@ struct SettingsView: View {
     // MARK: - Properties
     
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     // MARK: - Body
     var body: some View {
@@ -34,6 +35,36 @@ struct SettingsView: View {
                             Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins, and much more.")
                                 .font(.footnote)
                         }
+                    }
+                    
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish, you can restart the application by toogle thw switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
                     }
                     
                     GroupBox(label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
